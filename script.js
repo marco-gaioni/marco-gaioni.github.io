83a5d12bd9fc8f8eaec9edb8b3e13a26cd755f54
fillo-monta <59571682+fillo-monta@users.noreply.github.com>
2020-03-25 22:04:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const domande = [
 			{
 				risposta: "Rilevanza",
 				isCorrect: true,
+				descrizione: "rilevanza s. f. [der. di rilevante]. – Il fatto, la caratteristica di essere rilevante, cioè di notevole importanza o anche gravità, soprattutto riguardo a determinati fini.",
 			},
 		]
 	},
@@ -40,6 +41,7 @@ const domande = [
 			{
 				risposta: "L'emittente ufficiale del mondo arabo",
 				isCorrect: true,
+				descrizione: "Al Jazeera è una rete televisiva satellitare con sede in Qatar, talvolta menzionata come Al Jazira.",
 			},
 		]
 	},
@@ -62,6 +64,7 @@ const domande = [
 			{
 				risposta: "Machiavelli",
 				isCorrect: true,
+				descrizione: "Mandragola è una commedia di Niccolò Machiavelli, considerata il capolavoro del teatro del Cinquecento e un classico della drammaturgia italiana. Composta da un prologo e cinque atti, è una potente satira sulla corruttibilità della società italiana dell'epoca.",
 			},
 		]
 	},
@@ -84,17 +87,19 @@ const domande = [
 			{
 				risposta: "Ha scritto l'Aminta",
 				isCorrect: true,
+				descrizione: "L'Aminta è una favola pastorale composta da Torquato Tasso nel 1573 e pubblicata nel 1580 circa. La prima rappresentazione ebbe luogo con buone probabilità al Belvedere di Ferrara",
 			},
 		]
 	},
 ];
 
 
-buttonsid = ["btn1","btn2","btn3","btn4"]
-vincite = ["500", "1.000", "1.500", "2.000", "3.000", "5.000", "7.000", "10.000", "15.000", "20.000", "30.000", "70.000", "150.000", "300.000", "1.000.000"]
+const buttonsid = ["btn1","btn2","btn3","btn4"];
+const vincite = ["500", "1.000", "1.500", "2.000", "3.000", "5.000", "7.000", "10.000", "15.000", "20.000", "30.000", "70.000", "150.000", "300.000", "1.000.000"];
 
 let indexDomanda=-1;
-const NUM_DOMANDE = 15;
+const NUM_DOMANDE = 1;
+let wannawin = 0;
 
 function shuffle(array) {
  	var currentIndex = array.length, temporaryValue, randomIndex;
@@ -146,6 +151,7 @@ function Timer(clearinterval=false){
 		if (t < 0) { 
 			clearInterval(timer);
 			countdown.innerHTML = "TEMPO SCADUTO"; 
+			temposcaduto();
 		} 
 	}, 1000); 
 };
@@ -172,10 +178,14 @@ function Verifica(indexDomanda, buttonid){
 	document.getElementById("debug").innerHTML=rispostaselzionata;
 	risposteList = domande[indexDomanda].risposte;
 	ret = false;
+	let scopri = document.getElementById("scopridipiu");
 	for (let i = 0; i < risposteList.length; i++) {
 	  if (risposteList[i].risposta === rispostaselzionata){
-			if (risposteList[i].isCorrect)
+			if (risposteList[i].isCorrect){
 				ret = risposteList[i].isCorrect;
+				scopri.innerHTML = risposteList[i].descrizione;
+				++ wannawin;
+			};
 		};
 	}
 	return ret;
@@ -189,17 +199,20 @@ function NextDomanda(buttonid=0){
 		let x = buttonsid.splice(i,1);
 		buttonsid.forEach((e, i, arr) => document.getElementById(e).disabled = true);
 
-		if (Verifica(indexDomanda, buttonid)){
+		if (Verifica(indexDomanda, buttonid) && wannawin<NUM_DOMANDE){
+			let x = document.getElementById("haivintotot");
+			x.innerHTML = "Complimenti! Hai vinto " + vincite[indexDomanda] +"$. Vuoi approfondire l'argomanto? Clicca il bottone qui sotto.";
 			$('#giusto').modal('show');
 			//document.getElementById("giusto").style.display = "block";
-			document.getElementById("sbagliato").style.display = "none";
 			document.getElementById("proxdomanda").style.display = "block";
 			BarIncrement(indexDomanda);
 		}
+		else if (wannawin === NUM_DOMANDE){
+			win();
+		}
 		else{
-			document.getElementById("sbagliato").style.display = "block";
+			$('#sbagliato').modal('show');
 			//document.getElementById("giusto").style.display = "none";
-			GameOver()
 		} 
 		buttonsid.push(buttonid);
 	}
@@ -208,31 +221,39 @@ function NextDomanda(buttonid=0){
 };
 
 function ProxDom(){
+	document.getElementById("proxdomanda").style.display = "none";
 	buttonsid.forEach((e, i, arr) => document.getElementById(e).disabled = false);
 	Populate(dom, ++indexDomanda);
 	Timer();
-}
+};
 
-
-function GameOver(){
-	Timer(true);
-	document.getElementById("gameover").style.display = "block";
-	document.getElementById("rigioca").style.display = "block";
-}
 
 function Rigioca(){
 	window.location.reload();
-}
+};
 
 
 function BarIncrement(indexDomanda){
 	const barra = document.getElementById("montepremiBar");
 	let currBar = 100 * (indexDomanda + 1)/NUM_DOMANDE;
 	barra.style.height = String(currBar) + "%" ;
-	barra.innerHTML = vincite[15*currBar/100-1];
+	barra.innerHTML = vincite[NUM_DOMANDE*currBar/100-1];
+};
+
+
+function win(){
+	let tutto = document.getElementById("tutto");
+	let haivinto = document.getElementById("vinto");
+	tutto.style.display = "none";
+	haivinto.style.display = "block";
 }
 
-
+function temposcaduto(){
+	let tutto = document.getElementById("tutto");
+	let perso = document.getElementById("perso");
+	tutto.style.display = "none";
+	perso.style.display = "block";
+}
 
 /*
 let maxTimer = 60;
@@ -250,8 +271,6 @@ if (t < 0) {
 	countdown.innerHTML = "TEMPO SCADUTO"; 
 } 
 }, 1000); 
-
-
 */
 /*let maxTimer = 60000;
 let y = 0
@@ -261,10 +280,8 @@ y += increment;
 let t = maxTimer - y; 
 let countdown = document.getElementById("countdown");
 let barra = document.getElementById("demo");
-
 if ((t%100)==0)
 	countdown.innerHTML = Math.floor(t/1000) + " sec"; 
-
 currBar = Math.floor(100*t/(maxTimer));
 barra.style.width = String(currBar) + "%";
 if (t <= 0) { 
